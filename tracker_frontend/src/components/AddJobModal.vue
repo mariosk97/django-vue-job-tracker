9162a29f61b4f0e33584d32150d6492ef7215112
Marios <mariosk97@yahoo.gr>
2025-06-04 23:06:50 +0300

    job modal updated

--- tracker_frontend/src/components/AddJobModal.vue
+++ tracker_frontend/src/components/AddJobModal.vue
@@ -1,10 +1,8 @@
-<!-- Modal -->
 <template> 
     <div v-if="isModalOpen" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
         <div class="bg-white p-6 rounded-lg w-full max-w-md shadow-lg">
             <h2 class="text-lg font-bold mb-4">Add New Job</h2>
 
-            <!-- Job Form -->
             <form class="space-y-4">
                 <input type="text" v-model="form.company" placeholder="Company" class="w-full border px-4 py-2 rounded" />
                 <input v-model="form.position" placeholder="Position" class="w-full border px-4 py-2 rounded" />
@@ -17,7 +15,6 @@
                 <input type="date" v-model="form.date_applied" class="w-full border px-4 py-2 rounded" />
             </form>
 
-            <!-- Buttons -->
             <div class="mt-6 flex justify-end gap-2">
                 <button @click="isModalOpen = false" class="px-4 py-2 bg-gray-300 rounded">Cancel</button>
                 <button @click="submitForm" class="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700">
@@ -27,4 +24,36 @@
         </div>
     </div>   
 </template>    
+
+<script>
+export default {
+  props: {
+    isModalOpen: Boolean
+  },
+
+  data() {
+    return {
+      form: {
+        company: '',
+        position: '',
+        status: 'applied',
+        date_applied: ''
+      }
+    }
+  },
+
+  methods: {
+    submitForm() {
+      axios.post('/api/jobs/', this.form)
+        .then(response => {
+          this.$emit('job-created')       // tell parent to reload jobs
+          this.$emit('close')             // tell parent to close modal
+          this.form = { company: '', position: '', status: 'applied', date_applied: '' }
+        })
+        .catch(error => console.log(error))
+    }
+  }
+}  
+
+</script>  
    
